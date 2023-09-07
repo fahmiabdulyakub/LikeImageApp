db1cd541dc41f0a20d474a15761f790e006f1f3f
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2023-09-07 23:29:43 +0700

    create alias path

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@themes': './src/themes',
+          '@services': './src/services',
+          '@utils': './src/utils',
+          '@types': './src/types',
+        },
+      },
+    ],
+  ],
 };
